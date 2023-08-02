e3a89caff17de713ccffc4684de8f26625deefc9
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-01 22:19:43 -0700

    Add top part of header

---
+++ src/icons/MenuIcon.tsx
@@ -0,0 +1,15 @@
+export const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path
+        fill="currentColor"
+        d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+      />
+    </svg>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import { CloseIcon } from "~/icons/CloseIcon";
+import { MenuIcon } from "~/icons/MenuIcon";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <div className="flex w-full max-w-2xl flex-col"></div>
+    <main className="flex min-h-screen flex-col items-center bg-black text-white">
+      <div className="flex w-full max-w-2xl flex-col">
+        <header className="flex h-14 items-center">
+          <button className="h-14 px-6">
+            <span className="sr-only">Open menu</span>
+            <MenuIcon />
+          </button>
+
+          <div className="relative flex grow">
+            <input className="grow bg-slate-800 px-1 py-2" />
+            <button className="absolute bottom-0 right-0 top-0 px-1 text-slate-300">
+              <span className="sr-only">Clear input</span>
+              <CloseIcon />
+            </button>
+          </div>
+
+          <button className="flex h-14 items-center justify-center px-6">
+            <span className="flex h-6 w-6 items-center justify-center rounded border-2 border-white text-lg">
+              <span className="sr-only">English search</span>
+              <span aria-hidden>E</span>
+            </span>
+          </button>
+        </header>
+      </div>
     </main>
   );
 }
