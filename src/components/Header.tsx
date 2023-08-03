dc007d2a745a14729053e9401a8fdeaaebef47ad
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-02 17:47:31 -0700

    User can switch between search languages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,8 @@ import { MicrophoneIcon } from "~/icons/MicrophoneIcon";
 import { PuzzlePieceIcon } from "~/icons/PuzzlePieceIcon";
 import { classNames } from "~/utils/classNames";
 
+type SearchLanguage = "English" | "Japanese";
+
 type HeaderTabId = (typeof headerTabs)[number]["id"];
 
 const DEFAULT_HEADER_TAB_ID: HeaderTabId = 3;
@@ -21,6 +23,9 @@ const headerTabs = [
 ] as const;
 
 export const Header = () => {
+  const [searchLanguage, setSearchLanguage] =
+    useState<SearchLanguage>("English");
+
   const [headerTabId, setHeaderTabId] = useState<HeaderTabId>(
     DEFAULT_HEADER_TAB_ID
   );
@@ -41,10 +46,24 @@ export const Header = () => {
           </button>
         </div>
 
-        <button className="flex h-full items-center justify-center px-6">
-          <span className="flex h-6 w-6 items-center justify-center rounded border-2 border-white text-lg">
-            <span className="sr-only">English search</span>
-            <span aria-hidden>E</span>
+        <button
+          className="flex h-full items-center justify-center px-6"
+          onClick={() =>
+            setSearchLanguage((searchLanguage) =>
+              searchLanguage === "English" ? "Japanese" : "English"
+            )
+          }
+        >
+          <span
+            className={classNames(
+              "flex h-6 w-6 items-center justify-center rounded border-2 border-white text-lg font-bold",
+              searchLanguage === "English"
+                ? "bg-black text-white"
+                : "bg-white text-black"
+            )}
+          >
+            <span className="sr-only">{searchLanguage} search</span>
+            <span aria-hidden>{searchLanguage[0]}</span>
           </span>
         </button>
       </section>
