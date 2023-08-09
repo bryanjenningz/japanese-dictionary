d4089aa238cab986dbcd9af3a18ac6efed939cc9
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-09 14:06:04 -0700

    Add debounce

---
+++ src/utils/debounce.ts
@@ -0,0 +1,10 @@
+export const debounce = <T extends unknown[]>(
+  fn: (...args: T) => void,
+  delay: number
+): ((...args: T) => void) => {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined = undefined;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+};
