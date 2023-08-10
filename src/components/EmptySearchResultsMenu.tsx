888de16ad68f02dc2c77807093d4343269554483
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-10 15:21:10 -0700

    Add history link on empty search results menu

--- src/components/EmptySearchResultsMenu.tsx
+++ src/components/EmptySearchResultsMenu.tsx
@@ -3,6 +3,7 @@ import { classNames } from "~/utils/classNames";
 import { SearchTipsModal } from "~/components/SearchTipsModal";
 import { type DarkModeState, useDarkModeStore } from "~/stores/darkModeStore";
 import { useStore } from "~/stores/useStore";
+import Link from "next/link";
 
 export const EmptySearchResultsMenu = () => {
   const isDarkMode = useStore<DarkModeState, DarkModeState["isDarkMode"]>(
@@ -13,25 +14,37 @@ export const EmptySearchResultsMenu = () => {
 
   const emptySearchResultsMenu = [
     { label: "Browse Entries" },
-    { label: "History" },
+    { label: "History", href: "/history" },
     { label: "Search Tips", onClick: () => setIsSearchTipsModalShown(true) },
   ];
 
   return (
     <>
       <ul>
-        {emptySearchResultsMenu.map(({ label, onClick }) => {
+        {emptySearchResultsMenu.map(({ label, onClick, href }) => {
           return (
             <li key={label}>
-              <button
-                className={classNames(
-                  "w-full border-b p-4 text-left text-lg",
-                  isDarkMode ? "border-slate-700" : "border-slate-300"
-                )}
-                onClick={onClick}
-              >
-                {label}
-              </button>
+              {href ? (
+                <Link
+                  href={href}
+                  className={classNames(
+                    "block w-full border-b p-4 text-left text-lg",
+                    isDarkMode ? "border-slate-700" : "border-slate-300"
+                  )}
+                >
+                  {label}
+                </Link>
+              ) : (
+                <button
+                  className={classNames(
+                    "block w-full border-b p-4 text-left text-lg",
+                    isDarkMode ? "border-slate-700" : "border-slate-300"
+                  )}
+                  onClick={onClick}
+                >
+                  {label}
+                </button>
+              )}
             </li>
           );
         })}
