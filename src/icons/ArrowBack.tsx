675e9698c4fd3cd256f907e27c54ce2bd90a1a07
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-11 13:41:40 -0700

    Add arrow back icon

---
+++ src/icons/ArrowBack.tsx
@@ -0,0 +1,15 @@
+export const ArrowBackIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path
+        fill="currentColor"
+        d="m326.152-434.5 218.739 218.739L480-151.869 151.869-480 480-808.131l64.891 63.892L326.152-525.5h481.979v91H326.152Z"
+      />
+    </svg>
+  );
+};
