fd312179875584e2a3710f7917f0708690c1a41f
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-13 14:48:05 -0700

    Add long press menu states in useLongPress

--- src/utils/useLongPress.ts
+++ src/utils/useLongPress.ts
@@ -1,15 +1,23 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+type LongPressMenu =
+  | { type: "CLOSED" }
+  | { type: "PRESSED_DOWN" }
+  | { type: "OPEN"; item: HTMLElement };
 
 export const useLongPress = (onLongPress: () => void) => {
+  const [menu, setMenu] = useState<LongPressMenu>({ type: "CLOSED" });
   const timeoutId = useRef<undefined | ReturnType<typeof setTimeout>>();
 
   const onTouchStart = () => {
+    setMenu({ type: "PRESSED_DOWN" });
     timeoutId.current = setTimeout(onLongPress, 500);
   };
 
-  const onTouchEnd = () => {
+  const onTouchEnd = (item: HTMLElement) => {
+    setMenu({ type: "OPEN", item });
     clearTimeout(timeoutId.current);
   };
 
-  return { onTouchStart, onTouchEnd };
+  return { menu, onTouchStart, onTouchEnd };
 };
