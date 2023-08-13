7c038330b01032ab53a2fdf62b250bc506e1f697
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-12 17:02:21 -0700

    Add chevron right icon

---
+++ src/icons/ChevronRightIcon.tsx
@@ -0,0 +1,15 @@
+export const ChevronRightIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path
+        fill="currentColor"
+        d="M321-72.348 242.348-151l329-329-329-329L321-887.652 728.652-480 321-72.348Z"
+      />
+    </svg>
+  );
+};
