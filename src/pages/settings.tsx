eeddf598af38c5b627f752a3e5752a42ebf4b9d8
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-16 02:53:11 -0700

    Add dark mode to settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -10,7 +10,16 @@ export default function Settings() {
     useDarkModeStore,
     (x) => x.isDarkMode
   );
+  const setIsDarkMode = useDarkModeStore((x) => x.setIsDarkMode);
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const settings = [
+    {
+      name: "Dark mode",
+      value: !!isDarkMode,
+      toggle: () => setIsDarkMode(!isDarkMode),
+    },
+  ];
 
   return (
     <main
@@ -26,7 +35,33 @@ export default function Settings() {
         closeSideMenu={() => setIsSideMenuOpen(false)}
       />
 
-      <div className="w-full max-w-2xl"></div>
+      <ul className="w-full max-w-2xl">
+        {settings.map(({ name, toggle, value }) => {
+          return (
+            <li key={name}>
+              <label className="flex justify-between p-4 text-lg">
+                <span className="flex grow flex-col">
+                  <span>{name}</span>
+                  <span
+                    className={classNames(
+                      "text-sm",
+                      isDarkMode ? "text-slate-400" : "text-slate-500"
+                    )}
+                  >
+                    {value ? "Enabled" : "Disabled"}
+                  </span>
+                </span>
+                <input
+                  className="w-5"
+                  type="checkbox"
+                  checked={value}
+                  onChange={toggle}
+                />
+              </label>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
