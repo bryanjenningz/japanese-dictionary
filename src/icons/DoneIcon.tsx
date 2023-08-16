4e8b4a6ae2f2ef9aad67f52935c559077cfa6a8d
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-15 20:39:36 -0700

    Add done icon

---
+++ src/icons/DoneIcon.tsx
@@ -0,0 +1,15 @@
+export const DoneIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path
+        fill="currentColor"
+        d="M382-232.348 146.348-468l64.891-64.891L382-362.131l366.761-366.76L813.652-664 382-232.348Z"
+      />
+    </svg>
+  );
+};
