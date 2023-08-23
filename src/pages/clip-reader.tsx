e07e3473a84768dd70253e37f430785ec24a786a
Bryan Jennings <bryanjenningz@gmail.com>
2023-08-22 21:45:02 -0700

    Add React hooks for clip text and selected text for clip reader page

--- src/pages/clip-reader.tsx
+++ src/pages/clip-reader.tsx
@@ -14,71 +14,21 @@ import { classNames } from "~/utils/classNames";
 const MAX_WORD_SIZE = 20;
 
 export default function ClipReader() {
-  const addClipReaderLookup = useHistoryStore((x) => x.addClipReaderLookup);
   const isDarkMode = useStore(useDarkModeStore, (x) => x.isDarkMode);
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-
-  const clipReaderTexts = useStore(
-    useClipReaderTextStore,
-    (x) => x.clipReaderTexts
-  );
-  const selectedClipReaderText = useStore(
-    useClipReaderTextStore,
-    (x) => x.selectedClipReaderText
-  );
-  const clipText =
-    selectedClipReaderText?.text ?? clipReaderTexts?.[0]?.text ?? "";
-
-  const search = useSearch();
+  const clipText = useClipText();
+  const {
+    selectedText,
+    wordEntries,
+    searchText,
+    selectedTextIndex,
+    setSelectedTextIndex,
+  } = useSelectedText(clipText);
   const selectedTextElement = useRef<HTMLButtonElement | null>(null);
-
-  const [selectedTextIndex, setSelectedTextIndex] = useState<null | number>(
-    null
-  );
-
-  const { selectedTextLength, wordEntries } = ((): WordSearchResult => {
-    if (selectedTextIndex === null) {
-      return { selectedTextLength: 1, wordEntries: [] };
-    }
-
-    const text = clipText
-      .slice(selectedTextIndex, selectedTextIndex + MAX_WORD_SIZE)
-      .trim();
-
-    return search(text);
-  })();
-
-  const searchText =
-    selectedTextIndex === null
-      ? ""
-      : clipText.slice(
-          selectedTextIndex,
-          selectedTextIndex + selectedTextLength
-        );
-
-  useEffect(() => {
-    if (wordEntries[0] && searchText) {
-      addClipReaderLookup({
-        time: Date.now(),
-        searchText,
-        resultIndex: 0,
-        wordEntry: wordEntries[0],
-      });
-    }
-  }, [addClipReaderLookup, wordEntries, searchText]);
-
   const selectedTextElementBottom = selectedTextElement.current
     ? selectedTextElement.current.getBoundingClientRect().bottom +
       window.scrollY
     : undefined;
-
-  const selectedText =
-    typeof selectedTextIndex === "number"
-      ? clipText.slice(
-          selectedTextIndex,
-          selectedTextIndex + selectedTextLength
-        )
-      : "";
 
   return (
     <main
@@ -113,7 +63,7 @@ export default function ClipReader() {
           selectedTextIndex={selectedTextIndex}
           selectedTextElement={selectedTextElement}
           setSelectedTextIndex={setSelectedTextIndex}
-          selectedTextLength={selectedTextLength}
+          selectedTextLength={selectedText.length}
         />
 
         <WordEntryList
@@ -124,3 +74,65 @@ export default function ClipReader() {
     </main>
   );
 }
+
+const useClipText = () => {
+  const clipReaderTexts = useStore(
+    useClipReaderTextStore,
+    (x) => x.clipReaderTexts
+  );
+  const selectedClipReaderText = useStore(
+    useClipReaderTextStore,
+    (x) => x.selectedClipReaderText
+  );
+  const clipText =
+    selectedClipReaderText?.text ?? clipReaderTexts?.[0]?.text ?? "";
+  return clipText;
+};
+
+const useSelectedText = (clipText: string) => {
+  const [selectedTextIndex, setSelectedTextIndex] = useState<null | number>(
+    null
+  );
+  const search = useSearch();
+  const { selectedTextLength, wordEntries } = ((): WordSearchResult => {
+    if (selectedTextIndex === null) {
+      return { selectedTextLength: 1, wordEntries: [] };
+    }
+    const text = clipText
+      .slice(selectedTextIndex, selectedTextIndex + MAX_WORD_SIZE)
+      .trim();
+    return search(text);
+  })();
+  const searchText =
+    selectedTextIndex === null
+      ? ""
+      : clipText.slice(
+          selectedTextIndex,
+          selectedTextIndex + selectedTextLength
+        );
+  const addClipReaderLookup = useHistoryStore((x) => x.addClipReaderLookup);
+  useEffect(() => {
+    if (wordEntries[0] && searchText) {
+      addClipReaderLookup({
+        time: Date.now(),
+        searchText,
+        resultIndex: 0,
+        wordEntry: wordEntries[0],
+      });
+    }
+  }, [addClipReaderLookup, wordEntries, searchText]);
+  const selectedText =
+    typeof selectedTextIndex === "number"
+      ? clipText.slice(
+          selectedTextIndex,
+          selectedTextIndex + selectedTextLength
+        )
+      : "";
+  return {
+    selectedText,
+    wordEntries,
+    searchText,
+    selectedTextIndex,
+    setSelectedTextIndex,
+  };
+};
