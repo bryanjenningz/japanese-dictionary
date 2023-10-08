30f7f489978f66d9ce6271f3759acc04c4fb9300
Bryan Jennings <bryanjenningz@gmail.com>
2023-10-08 01:40:41 -0700

    Change theme color and name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Japleco</title>
+        <title>React Pleco</title>
         <meta
           name="description"
           content="A Japanese Pleco clone web app written with React, TypeScript, Next.js, TailwindCSS, and Zustand."
         />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#333" />
+        <meta name="theme-color" content="#000" />
         <link rel="manifest" href="/app.webmanifest" />
       </Head>
       <Component {...pageProps} />
